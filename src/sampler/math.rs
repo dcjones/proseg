202f4318aeb6f2b17edc8e3757bebdde5b1d97e6
Daniel C. Jones <djones3@fredhutch.org>
2024-07-14 17:26:44 -0700

    Solve collapsing components by using NB marginal.

--- src/sampler/math.rs
+++ src/sampler/math.rs
@@ -11,6 +11,9 @@ use rand_distr::StandardNormal;
 //     x * x
 // }
 
+pub fn odds_to_prob(q: f32) -> f32 {
+    q / (1.0 + q)
+}
 
 pub fn relerr(a: f32, b: f32) -> f32 {
     ((a - b) / a).abs()
@@ -32,13 +35,13 @@ pub fn normal_x2_logpdf(σ: f32, x2: f32) -> f32 {
     -x2 / (2.0 * σ.powi(2)) - σ.ln() - LN_SQRT_TWO_PI
 }
 
-pub fn gamma_logpdf(shape: f32, scale: f32, x: f32) -> f32 {
-    return
-        -lgammaf(shape)
-        - shape * scale.ln()
-        + (shape - 1.0) * x.ln()
-        - x / scale;
-}
+// pub fn gamma_logpdf(shape: f32, scale: f32, x: f32) -> f32 {
+//     return
+//         -lgammaf(shape)
+//         - shape * scale.ln()
+//         + (shape - 1.0) * x.ln()
+//         - x / scale;
+// }
 
 pub fn rand_crt(rng: &mut ThreadRng, n: u32, r: f32) -> u32 {
     (0..n)
@@ -46,35 +49,35 @@ pub fn rand_crt(rng: &mut ThreadRng, n: u32, r: f32) -> u32 {
         .sum()
 }
 
-// pub fn negbin_logpmf(r: f32, lgamma_r: f32, p: f32, k: u32) -> f32 {
-//     let k_ln_factorial = lgammaf(k as f32 + 1.0);
-//     let lgamma_rpk = lgammaf(r + k as f32);
-//     return negbin_logpmf_fast(r, lgamma_r, lgamma_rpk, p, k, k_ln_factorial);
-// }
+pub fn negbin_logpmf(r: f32, lgamma_r: f32, p: f32, k: u32) -> f32 {
+    let k_ln_factorial = lgammaf(k as f32 + 1.0);
+    let lgamma_rpk = lgammaf(r + k as f32);
+    return negbin_logpmf_fast(r, lgamma_r, lgamma_rpk, p, k, k_ln_factorial);
+}
 
-// fn negbin_logpmf_fast(
-//     r: f32,
-//     lgamma_r: f32,
-//     lgamma_rpk: f32,
-//     p: f32,
-//     k: u32,
-//     k_ln_factorial: f32,
-// ) -> f32 {
-//     const MINP: f32 = 0.999999_f32;
-//     let p = p.min(MINP);
+fn negbin_logpmf_fast(
+    r: f32,
+    lgamma_r: f32,
+    lgamma_rpk: f32,
+    p: f32,
+    k: u32,
+    k_ln_factorial: f32,
+) -> f32 {
+    const MINP: f32 = 0.999999_f32;
+    let p = p.min(MINP);
 
-//     if k == 0 {
-//         // handle common case in sparse data efficiently
-//         // r * (1.0 - p).ln()
-//         r * (-p).ln_1p()
-//     } else {
-//         lgamma_rpk
-//             - lgamma_r
-//             - k_ln_factorial
-//             // + (k as f32) * p.ln() + r * (1.0 - p).ln()
-//             + (k as f32) * p.ln() + r * (-p).ln_1p()
-//     }
-// }
+    if k == 0 {
+        // handle common case in sparse data efficiently
+        // r * (1.0 - p).ln()
+        r * (-p).ln_1p()
+    } else {
+        lgamma_rpk
+            - lgamma_r
+            - k_ln_factorial
+            // + (k as f32) * p.ln() + r * (1.0 - p).ln()
+            + (k as f32) * p.ln() + r * (-p).ln_1p()
+    }
+}
 
 // const SQRT2: f32 = 1.4142135623730951_f32;
 
